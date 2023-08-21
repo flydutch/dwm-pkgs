f38560c9f791cabd366be94575ca4f0a627058f0
Matteo Foppiano <matteo@redsplat.org>
2023-08-21 10:54:42 +0200

    dwmblocks: fix scripts' path

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* icon*, command, update interval, update signal */
-	{"🗓 ", "date '+%b %d (%a) %I:%M:%S %p %z'", 1, 0},
+	{"", "$HOME/bin/statusbar/sb-volume", 0, 10},
+	{"", "$HOME/bin/statusbar/sb-battery", 5, 3},
+	{"", "$HOME/bin/statusbar/sb-internet", 5, 4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 2;
