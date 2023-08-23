843c693fcefeb858351369503e3829e25db3017f
Matteo Foppiano <matteo@redsplat.org>
2023-08-23 14:37:38 +0200

    dwmblocks: remove statuscmd patch

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* icon*, command, update interval, update signal */
+	{"", "date '+%b %d (%a) %I:%M:%S %p %z'", 1, 0},
 	{"", "$HOME/bin/statusbar/sb-volume", 0, 10},
-	{"", "$HOME/bin/statusbar/sb-battery", 5, 3},
-	{"", "$HOME/bin/statusbar/sb-internet", 5, 4},
+	{"", "$HOME/bin/statusbar/sb-battery", 0, 3},
+	{"", "$HOME/bin/statusbar/sb-internet", 0, 4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
